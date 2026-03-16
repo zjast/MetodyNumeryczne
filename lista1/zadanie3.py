feee43ddbaa26dd0d787309631d3abf8447e2616
zjast <287305@student.pwr.edu.pl>
2026-03-16 16:21:24 +0100

    plots for 3,4

--- lista1/zadanie3.py
+++ lista1/zadanie3.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 zbior_x = [10.0**i for i in range(4, 11)]   
 
@@ -8,6 +9,16 @@ def funkcja1(x):
 def funkcja2(x):
     return(-1/(x+np.sqrt(1+x**2)))
 
-for i in zbior_x:
-    roznica = abs(funkcja1(i)-funkcja2(i))
-    print(f"({funkcja1(i)}, {funkcja2(i)}), roznica: {roznica}")
+y1 = np.array([funkcja1(x) for x in zbior_x])
+y2 = np.array([funkcja2(x) for x in zbior_x])
+roznica = np.abs(y1-y2)
+print(f"({y1}, {y2}), roznica: {roznica}")
+
+
+plt.figure(figsize=(10, 5))
+plt.plot(zbior_x, roznica)
+plt.title('Błąd numeryczny')
+plt.xlabel('x')
+plt.ylabel('błąd')
+plt.grid(True, alpha=0.3)
+plt.show()
--- lista1/zadanie4.py
+++ lista1/zadanie4.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 przedzial = np.linspace(1 - 1e-3, 1 + 1e-3, 100)
 
@@ -8,8 +9,15 @@ def funkcja1(x):
 def funkcja2(x):
     return(x**4 - 4*x**3 + 6*x**2 - 4*x +1)
 
-for x in przedzial:
-    roznica = abs(funkcja1(x) - funkcja2(x))
-    print(f"({funkcja1(x)}, {funkcja2(x)}, roznica: {roznica})")
+y1 = funkcja1(przedzial)
+y2 = funkcja2(przedzial)
+roznica = np.abs(y1 - y2)
 
 
+plt.figure(figsize=(10, 5))
+plt.plot(przedzial, roznica, color='red', linewidth=0.8)
+plt.title('Błąd numeryczny')
+plt.xlabel('x')
+plt.ylabel('roznica')
+plt.grid(True, alpha=0.3)
+plt.show()
